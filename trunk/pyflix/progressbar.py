aa21750f32310f3ca893c0dbc25f746b9e07ce14
george.sakkis <george.sakkis@9e0c86ed-a01c-0410-97cc-1de4ee9595b8>
2006-11-22 04:09:49 +0000

    1st Netflix prize import

---
+++ trunk/pyflix/progressbar.py
@@ -0,0 +1,169 @@
+'''Provides progress bar capabilities to console programs.
+
+@sort: ProgressBar,IterableProgressBar,FileProgressBar
+'''
+
+__all__ = ['ProgressBar', 'IterableProgressBar', 'FileProgressBar', 'progress']
+__author__ = 'George Sakkis <george.sakkis AT gmail.com>'
+
+import sys
+from array import array
+
+#====== ProgressBar ===========================================================
+
+class ProgressBar(object):
+    '''Basic progress bar class.
+
+    Allows a client to display the progress of a procedure in the form of a
+    progress bar as well as the actual percentage done, rounded to an integer::
+        [*************                                ] 33%
+
+    The client L{initializes <__init__>} a ProgressBar by specifying a minimum
+    (usually 0) and a maximum (e.g. number of procedure steps, file size,
+    etc.) number, and optionally the width of the progress bar and the output
+    stream to display it (standard error by default).
+    '''
+
+    def __init__(self, min, max, width=79, stream=sys.stderr):
+        '''        
+        @param min,max: The minimum and maximum bounds for the iteration.
+        @type min,max: Number (int or float)
+        '''
+        if min >= max:
+            raise ValueError('min value should be lower than max')
+        self._bar = array('c', '[]')
+        self._out = stream
+        self._width = width - len(self._bar)
+        self._min,self._max = map(float, (min,max))
+        self._span = self._max - self._min
+        for _ in xrange(self._width):
+            self._bar.insert(1,' ')
+        self.reset()
+
+    def reset(self):
+        '''To be called before reusing this ProgressBar.'''
+        self._amount = self._min
+        self._numHashes = self._roundedPercentDone = 0
+        for i in xrange(1,self._width+1):
+            self._bar[i] = ' '
+        del self._bar[self._width+2:]
+
+    def update_to(self, amount):
+        '''Inform this progress bar about the progress of the displayed procedure.
+
+        @param amount: A number between the min and max specified in L{__init__}.
+        '''
+        self._amount = amount
+        percentDone = (amount-self._min) / self._span
+        new_roundedPercentDone = int(round(percentDone*100))
+        new_numHashes = int(round(percentDone*self._width))
+        changed = False
+        if self._numHashes < new_numHashes:
+            for i in xrange(self._numHashes, new_numHashes):
+                self._bar[i+1] = '*'
+            self._numHashes = new_numHashes
+            changed = True
+        if self._roundedPercentDone < new_roundedPercentDone:
+            self._roundedPercentDone = new_roundedPercentDone
+            percentString = array('c', ' %d%%' % new_roundedPercentDone)
+            startIndex = self._width+2
+            self._bar[startIndex:startIndex+len(percentString)] = percentString
+            changed = True
+        if changed:
+            print >> self._out, self._bar.tostring(), '\r',
+            self._out.flush()
+
+    def update_by(self, amount=1):
+        '''Inform this progress bar about the progress of the displayed procedure.
+
+        @param amount: A number that indicates how much has the procedure
+            advanced since the previous call (or the beginning if this is the
+            first call).
+        '''
+        self.update_to(self._amount+amount)
+
+    def irange(self, step=1, refresh=None, relative=False):
+        '''Iterate over the range(min,max,step) updating this progress bar every
+        C{refresh}.
+        
+        @param step: The step of the iteration.
+        @type step: Number (int or float)
+        @param refresh: Determines how often should the progress bar reflect
+            the progress of the procedure. Typically a multiple of C{step}, or
+            None for equal to C{step}.
+        @param relative: True if C{refresh} denotes a fraction of the whole
+            procedure (e.g. 1%) or False for absolute refresh.
+        '''
+        self.reset()
+        if refresh is None:
+            refresh = step
+        elif relative:
+            if not (0 < refresh < 1):
+                raise ValueError('Relative refresh must be a percentage')
+            refresh *= self._max-self._min
+        current = self._min; last_updated = -1
+        while (current-self._max)*step < 0:
+            # XXX: don't use 'amount % refresh == 0' because refresh maybe float < 1
+            div = current // refresh
+            if div != last_updated:
+                self.update_to(current)                 
+                last_updated = div
+            yield current
+            current += step
+        # update one final time after completion
+        self.update_to(current)
+        print >> self._out
+    
+#====== IterableProgressBar ====================================================
+
+class IterableProgressBar(ProgressBar):
+    '''A L{ProgressBar} that wraps an iterable.'''
+
+    def __init__(self, iterable, size=None, width=79, stream=sys.stderr):
+        '''
+        @param size: The length of the iterable. If None, then the size is
+            determined by attempting to call C{len}, or if this fails, by
+            iterating over the iterable.
+        '''
+        self._iterable = iterable
+        if size is None:
+            size = len(iterable)
+        ProgressBar.__init__(self, 0, size, width, stream)
+
+    def __iter__(self):
+        return self.progress()
+
+    def progress(self, refresh=1):
+        next = iter(self._iterable).next
+        for _ in self.irange(1, refresh, relative=refresh<1):
+            yield next()
+
+#====== FileProgressBar ========================================================
+
+class FileProgressBar(IterableProgressBar):
+    '''An L{IterableProgressBar} that wraps a file.'''
+
+    def __init__(self, path, width=79, stream=sys.stderr):
+        '''
+        @param path: The path of the file to be monitored.
+        '''
+        self._path = path
+        size = sum(1 for line in open(path))
+        IterableProgressBar.__init__(self, open(path), size, width, stream)
+
+    def reset(self):
+        IterableProgressBar.reset(self)
+        self._iterable = open(self._path)
+
+#====== iterprogress ===========================================================
+
+def progress(iterable, size=None, refresh=1, width=79, stream=sys.stderr):
+    return IterableProgressBar(iterable,size,width,stream).progress(refresh)
+
+if __name__ == '__main__':
+    import time
+    x = []
+    for i in progress('abcdefg'*2, width=70, refresh=1):
+        time.sleep(.1)
+        x.append(i)
+    print x
---
+++ trunk/pyflix/records.py
@@ -0,0 +1,244 @@
+__all__ = ['Record', 'RatedRecord', 'UnratedRecord',
+           'UnratedMovieRecord', 'UnratedUserRecord',
+           'RatedMovieRecord', 'RatedUserRecord']
+
+import mmap
+from struct import pack
+from itertools import izip
+import numpy as N
+from path import path
+
+from progressbar import ProgressBar
+
+#====== types pertinent to the binary files sorted by movie and by user ========
+
+MOVIE_ID_TYPECODE = 'H' # unsigned short
+USER_ID_TYPECODE  = 'L' # unsigned long
+
+class _MovieTypes(object):
+    _KeyDtype   = MOVIE_ID_TYPECODE       
+    _ValueDtype = USER_ID_TYPECODE    
+    # XXX: N.repeat requires the second arg to be convertible to int32
+    _CountDtype = 'l'       # number of users that rated a single movie
+    
+class _UserTypes(object):        
+    _KeyDtype   = USER_ID_TYPECODE
+    _ValueDtype = MOVIE_ID_TYPECODE
+    _CountDtype = 'H'      # number of movies rated by a single user
+
+
+#====== Base abstract record type ==============================================
+
+class Record(object):
+    __slots__ = ('id', '_values')
+
+    def __init__(self, key, values, counts):
+        raise NotImplementedError('Abstract class')
+
+    @classmethod
+    def fromfile(cls, datfile, readbuffer=128*1024**2):
+        from struct import unpack
+        size = path(datfile).size
+        f = open(datfile,'rb+',readbuffer)
+        m = mmap.mmap(f.fileno(), size, 
+                      access=mmap.ACCESS_READ)
+        kdtype, cdtype, vdtype = cls._KeyDtype, cls._CountDtype, cls._ValueDtype
+        ksize, csize, vsize = [N.dtype(t).itemsize for t in kdtype,cdtype,vdtype]
+        num_partitions = cls._num_partitions()
+        csize *= num_partitions
+        bar = ProgressBar(0, size, width=70)
+        read = m.read
+        for key in iter(lambda:read(ksize), ''):
+            # 1. read the key
+            key = unpack(kdtype,key)[0]
+            # 2. read the partition counts
+            counts = N.fromstring(read(csize), cdtype, num_partitions)
+            # 3. read the values
+            num_values = N.sum(counts)
+            values = N.fromstring(read(vsize*num_values), vdtype, num_values)
+            bar.update_to(m.tell())
+            yield cls(key, values, counts)
+
+    @classmethod
+    def fromvalues(cls, key, partitioned_values):
+        counts = map(len,partitioned_values)
+        values = []
+        for v in partitioned_values:
+            v.sort() # sort the values in each partition
+            values.extend(v)
+        return cls(key, values, counts)
+
+    def values(self):        
+        raise NotImplementedError
+                    
+    @staticmethod
+    def commonValues(*records):
+        '''Return the common values of the given records.'''
+        return intersect_arrays(*[record.values() for record in records])
+
+    @classmethod
+    def _num_partitions(cls):
+        raise NotImplementedError
+
+#====== Unrated record types ===================================================
+
+class UnratedRecord(Record):
+    '''A simple record type composed of a key (id) and an array of values.'''
+    
+    def __init__(self, key, values, counts=None):
+        self.id = key
+        self._values = N.array(values, self._ValueDtype)
+    
+    def values(self):        
+        return self._values
+                            
+    def tostring(self):
+        return ''.join([pack(self._KeyDtype, self.id),
+                        pack(self._CountDtype, len(self._values)),
+                        self._values.tostring()])
+        
+    def __str__(self):
+        return str((self.id, self._values))
+
+    @classmethod
+    def _num_partitions(cls):
+        return 1
+
+class UnratedMovieRecord(UnratedRecord, _MovieTypes):
+    pass
+
+class UnratedUserRecord(UnratedRecord, _UserTypes):
+    pass
+
+
+#====== Rated record types =====================================================
+
+class RatedRecord(Record):
+
+    __slots__ = Record.__slots__ + ('_counts',)
+        
+    def __init__(self, key, values, counts):
+        self.id = key
+        self._values = N.array(values, self._ValueDtype)
+        self._counts = N.array(counts, self._CountDtype)
+    
+    #--------- accessors -------------------------------------------------------
+                    
+    def values(self, min_rating=None, max_rating=None):        
+        if min_rating is max_rating is None: # optimize default case
+            return self._values
+        value_indices = N.add.accumulate(self._counts)
+        s = self._normalizeSlice(min_rating,max_rating)
+        if s.start > 0:
+            start = value_indices[s.start-1]
+        else:
+            start = 0
+        return self._values[start:value_indices[s.stop-1]]
+            
+    def ratings(self, min_rating=None, max_rating=None):
+        if min_rating is max_rating is None: # optimize default case
+            return N.repeat(self._AllRatings, self._counts)
+        s = self._normalizeSlice(min_rating,max_rating)
+        return N.repeat(self._AllRatings[s], self._counts[s])
+
+    def iterValueRatings(self, min_rating=None, max_rating=None):        
+        if min_rating is max_rating is None: # optimize default case
+            return izip(self._values, N.repeat(self._AllRatings,self._counts))
+        return izip(self.values(min_rating,max_rating),
+                    self.ratings(min_rating,max_rating))
+    
+    #--------- joining records -------------------------------------------------
+    
+    @staticmethod
+    def commonValues(*records, **min_max_ratings):
+        '''Return the common values of the given records.
+        
+        @param min_max_ratings: Optional 'min_rating' and 'max_rating' arguments
+        '''
+        args = [min_max_ratings.get(name) for name in 'min_rating','max_rating']
+        return intersect_arrays(*[record.values(*args) for record in records])
+    
+    @staticmethod
+    def jointRatings(*records, **min_max_ratings):
+        '''Return a 2-D array whose A[i,j] element is the rating of records[i]
+        for the j-th common value returned by C{common_values()}.
+        '''
+        return RatedRecord._jointValueRatings(*records, **min_max_ratings)[1]
+    
+    @staticmethod
+    def iterCommonValuesRatings(*records, **min_max_ratings):
+        '''Iterate over (common_value,ratings) tuples for each common_value 
+        returned by C{common_values()}, where ratings is the array of ratings
+        given for this common_value by all records.
+        '''
+        common_values, joint_ratings = RatedRecord._jointValueRatings(*records,
+                                                                  **min_max_ratings)
+        for i,value in enumerate(common_values):
+            yield (value, joint_ratings[:,i])
+    
+    #--------- string representation -------------------------------------------
+    
+    def tostring(self):
+        return ''.join([pack(self._KeyDtype, self.id),
+                        self._counts.tostring(), self._values.tostring()])
+        
+    def __str__(self):
+        return str((self.id, self._counts, self._values))
+        
+    #--------- internal attributes ---------------------------------------------
+
+    _AllRatings = N.arange(1,6,dtype=N.uint8)
+
+    @classmethod
+    def _num_partitions(cls):
+        return len(cls._AllRatings)
+        
+    def _normalizeSlice(self, min_rating, max_rating):
+        get_index = lambda x: N.where(self._AllRatings==x)[0][0]
+        if min_rating is not None:
+            min_rating = get_index(min_rating)
+        else:
+            min_rating = 0
+        if max_rating is not None:
+            max_rating = get_index(max_rating) + 1
+        else:
+            max_rating = len(self._AllRatings)
+        return slice(min_rating,max_rating)
+
+    @staticmethod
+    def _jointValueRatings(*records, **min_max_ratings):
+        args = [min_max_ratings.get(name) for name in 'min_rating','max_rating']
+        values_list = [record.values(*args) for record in records]
+        common_values = intersect_arrays(*values_list)
+        joint_ratings = N.empty((len(records),len(common_values)),
+                                dtype=RatedRecord._AllRatings.dtype)
+        if len(common_values):
+            for i,values in enumerate(values_list):
+                # sort the values because N.searchsorted requires it
+                s_indices = values.argsort()
+                # find the indices of the common values
+                c_indices = N.searchsorted(values[s_indices], common_values)
+                # take the ratings for the i-th record and rearrange them according to s_indices 
+                ratings = records[i].ratings(*args)[s_indices]
+                # and finally take the corresponding ratings for the common values
+                joint_ratings[i] = ratings[c_indices]
+        return (common_values, joint_ratings)
+
+
+class RatedMovieRecord(RatedRecord, _MovieTypes):
+    pass
+
+class RatedUserRecord(RatedRecord, _UserTypes):
+    pass
+
+
+#===============================================================================
+
+def intersect_arrays(*arrays):
+    '''Intersection of 1D arrays with unique elements.'''
+    current = arrays[0]
+    for i in xrange(1,len(arrays)):
+        aux = N.concatenate((current,arrays[i]))
+        aux.sort()
+        current = aux[aux[1:] == aux[:-1]]
+    return current
