89b8ac67d8c314aaa92f5460952aead51af8743e
george.sakkis <george.sakkis@9e0c86ed-a01c-0410-97cc-1de4ee9595b8>
2006-11-24 05:06:45 +0000

    - Added index file for each dat file (as pickle) - Dat files are mmaped; no records are loaded at startup. - New Index dict-like class to abstract index access. - Datasets don't require both movies and users (not all methods will work though if one is missing) - API changes:   * datasets.py: Renamed several public methods   * records.py: Replaced *movies/*users, with movies,users (accepts iterators too).   * Misc. refactoring

--- trunk/pyflix/datasets.py
+++ trunk/pyflix/datasets.py
@@ -1,35 +1,38 @@
-#!/usr/bin/env python
+__all__ = ['AbstractDataset', 'UnratedDataset', 'RatedDataset', 'writePredictions']
 
-__all__ = ['Dataset', 'RatedDataset']
+#==== imports ==================================================================
 
+import mmap
+import cPickle
 import numpy as N
 from path import path
 
-from records import *
-from records import MOVIE_ID_TYPECODE, USER_ID_TYPECODE
+from pyflix.records import *
+from pyflix.setup import iterQset
 
+#==== Abstract dataset =========================================================
 
-class Dataset(object):
+class AbstractDataset(object):
 
-    def __init__(self, dir, progressbar=True):
+    def __init__(self, dir):
         dir = path(dir)
-        # XXX: allocate an extra dummy movie at index 0 making the list effectively 1-based
-        self._movies = [None]
-        self._movies.extend(self._MovieRecordType.fromfile(dir/'movies.dat',
-                                                           progressbar=progressbar))
-        self._users = users = {}
-        for user in self._UserRecordType.fromfile(dir/'users.dat',
-                                                  progressbar=progressbar):
-            users[user.id] = user
+        self._movies = self._users = None
+        if (dir/'movies.idx').exists() and (dir/'movies.dat').exists():
+            self._movies = Index(dir/'movies.idx', dir/'movies.dat', self._MovieRecordType)
+        if (dir/'users.idx').exists() and (dir/'users.dat').exists():
+            self._users = Index(dir/'users.idx', dir/'users.dat', self._UserRecordType)
 
     def iterMoviesUsers(self):
         '''Return an iterator over all (movie,user) pairs of this dataset.'''
-        enum_movies = enumerate(self._movies)
-        enum_movies.next() # ignore the dummy movie
-        for movie_id, movie in enum_movies:
-            for user_id in movie.values():
-                yield (movie_id, user_id)
-
+        if self._movies is not None:
+            for movie_id, movie in self._movies.iteritems():
+                for user_id in movie.values():
+                    yield (movie_id, user_id)
+        else:
+            for user_id, user in self._users.iteritems():
+                for movie_id in user.values():
+                    yield (movie_id, user_id)
+            
     def movies(self, *users):
         '''Return an array of the movie IDs rated by the given user(s).
         
@@ -38,9 +41,9 @@ class Dataset(object):
         given users.
         '''
         if users:
-            return Record.commonValues(*[self._users[u] for u in users])
+            return AbstractRecord.commonValues(self._users[u] for u in users)
         else:
-            return N.arange(1, len(self._movies), dtype=MOVIE_ID_TYPECODE)
+            return N.array(self._movies.keys(), dtype=MOVIE_ID_TYPECODE)
     
     def users(self, *movies):
         '''Return an array of the user IDs that rated the given movie(s).
@@ -49,23 +52,33 @@ class Dataset(object):
         Otherwise, return the IDs of the users that have rated ALL the given movies.
         '''
         if movies:
-            return Record.commonValues(*[self._movies[m] for m in movies])
+            return AbstractRecord.commonValues(self._movies[m] for m in movies)
         else:
             return N.array(self._users.keys(), dtype=USER_ID_TYPECODE)
         
+    _MovieRecordType = NotImplemented
+    _UserRecordType = NotImplemented
+
+#==== Unrated dataset ==========================================================
+
+class UnratedDataset(AbstractDataset):
     _MovieRecordType = UnratedMovieRecord
     _UserRecordType = UnratedUserRecord
 
+#==== Rated dataset ==========================================================
 
-class RatedDataset(Dataset):
+class RatedDataset(AbstractDataset):
 
-    def iterMoviesUsersRatings(self, **min_max_ratings):
+    def iterMoviesUsersRatings(self, min_rating=None, max_rating=None):
         '''Return an iterator over all (movie,user,rating) tuples of this dataset.'''
-        enum_movies = enumerate(self._movies)
-        enum_movies.next() # ignore the dummy movie
-        for movie_id,movie in enum_movies:
-            for user_id, rating in movie.iterValueRatings(**min_max_ratings):
-                yield (movie_id, user_id, rating)
+        if self._movies is not None:
+            for movie_id,movie in self._movies.iteritems():
+                for user_id, rating in movie.iterValueRatings(min_rating,max_rating):
+                    yield (movie_id, user_id, rating)
+        else:
+            for user_id,user in self._users.iteritems():
+                for movie_id, rating in user.iterValueRatings(min_rating,max_rating):
+                    yield (movie_id, user_id, rating)
 
     def movies(self, *users, **min_max_ratings):
         '''Return an array of the movie IDs rated by the given user(s).
@@ -75,9 +88,9 @@ class RatedDataset(Dataset):
         given users.
         '''
         if users:
-            return RatedRecord.commonValues(*[self._users[u] for u in users],
+            return RatedRecord.commonValues((self._users[u] for u in users),
                                             **min_max_ratings)
-        else: return Dataset.movies(self)
+        else: return AbstractDataset.movies(self)
 
     def users(self, *movies, **min_max_ratings):
         '''Return an array of the user IDs that rated the given movie(s).
@@ -86,9 +99,9 @@ class RatedDataset(Dataset):
         Otherwise, return the IDs of the users that have rated ALL the given movies.
         '''
         if movies:
-            return RatedRecord.commonValues(*[self._movies[m] for m in movies],
+            return RatedRecord.commonValues((self._movies[m] for m in movies),
                                             **min_max_ratings)
-        else: return Dataset.users(self)
+        else: return AbstractDataset.users(self)
 
     def rmse(self, predictor):
         s,n = 0.0,0
@@ -100,78 +113,112 @@ class RatedDataset(Dataset):
 
     #---- ratings accessors and iterators for a single movie or user -----------
     
-    def movieRatings(self, movie, **min_max_ratings):
+    def ratingsTo(self, movie, min_rating=None, max_rating=None):
         '''Return an array of the given movie's ratings.'''
-        return self._movies[int(movie)].ratings(**min_max_ratings)
+        return self._movies[movie].ratings(min_rating,max_rating)
 
-    def userRatings(self, user, **min_max_ratings):
+    def ratingsBy(self, user, min_rating=None, max_rating=None):
         '''Return an array of the given user's ratings.'''
-        return self._users[user].ratings(**min_max_ratings)
+        return self._users[user].ratings(min_rating,max_rating)
     
-    def iterRatedUsers(self, movie, **min_max_ratings):
+    def iterUsersRatings(self, movie, min_rating=None, max_rating=None):
         '''Return an iterator over all (user,rating) pairs for the given movie.'''
-        return self._movies[movie].iterValueRatings(**min_max_ratings)
+        return self._movies[movie].iterValueRatings(min_rating,max_rating)
         
-    def iterRatedMovies(self, user, **min_max_ratings):
+    def iterMoviesRatings(self, user, min_rating=None, max_rating=None):
         '''Return an iterator over all (movie,rating) pairs for the given user.'''
-        return self._users[user].iterValueRatings(**min_max_ratings)
+        return self._users[user].iterValueRatings(min_rating,max_rating)
     
     #---- ratings accesors and iterators for jointly rated movies and users ----
     
-    def jointRatedMovieMatrix(self, *movies, **min_max_ratings):
+    def ratingsMatrixTo(self, *movies, **min_max_ratings):
         '''Return the ratings of the users that rated ALL the given movies.
         
-        Return a 2-D array whose [i,j] element is the rating of the i-th movie
-        by the j-th user returned from self.users(*movies).
+        Return a 2-D array whose [i,j] element is the rating of the j-th movie
+        by the i-th user returned from self.users(*movies).
         '''
-        movies = [self._movies[m] for m in movies]
-        return RatedRecord.jointRatings(*movies, **min_max_ratings)
+        return RatedRecord.jointRatings((self._movies[m] for m in movies),
+                                        **min_max_ratings)
 
-    def jointRatedUserMatrix(self, *users, **min_max_ratings):
+    def ratingsMatrixBy(self, *users, **min_max_ratings):
         '''Return the ratings of the movies that were rated by ALL the given users.
         
-        Return a 2-D array whose [i,j] element is the rating of the i-th user
-        to the j-th movie returned from self.movies(*users).
+        Return a 2-D array whose [i,j] element is the rating of the j-th user
+        to the i-th movie returned from self.movies(*users).
         '''
-        users = [self._users[u] for u in users]
-        return RatedRecord.jointRatings(*users, **min_max_ratings)
+        return RatedRecord.jointRatings((self._users[u] for u in users),
+                                        **min_max_ratings)
             
-    def iterJointlyRatedUsers(self, *movies, **min_max_ratings):
+    def iterJointUsersRatings(self, *movies, **min_max_ratings):
         '''Return an iterator over (user,ratings) pairs for each user that rated
         ALL the given movies. 'ratings' is a len(movies) 1-D array.
         '''
-        movies = [self._movies[m] for m in movies]
-        return RatedRecord.iterValueRatings(*movies, **min_max_ratings)
+        return RatedRecord.iterCommonValuesRatings(
+            (self._movies[m] for m in movies), **min_max_ratings)
     
-    def iterJointlyRatedMovies(self, *users, **min_max_ratings):
+    def iterJointMoviesRatings(self, *users, **min_max_ratings):
         '''Return an iterator over (movie,ratings) pairs for each movie rated by
         ALL the given users. 'ratings' is a len(users) 1-D array.
         '''
-        users = [self._users[u] for u in users]
-        return RatedRecord.iterValueRatings(*users, **min_max_ratings)
+        return RatedRecord.iterCommonValuesRatings(
+            (self._users[u] for u in users), **min_max_ratings)
         
     _MovieRecordType = RatedMovieRecord
     _UserRecordType = RatedUserRecord
 
+#==== writePredictions =========================================================
 
-##class MovieInfo(object):
-##    pass
-
-
-def writePredictionFile(infile, outfile, predictor, format='%.3f'):
-    from setup import iterQset
-    f = open(outfile, 'w')
+def writePredictions(qualifying_txt, output, predictor, format='%.3f'):
+    if not hasattr(output, 'read'):
+        output = open(output, 'w')
     last_movie_id = None
-    for movie_id,user_id in iterQset(infile):
+    for movie_id,user_id in iterQset(qualifying_txt):
         if movie_id != last_movie_id:
-            print >> f, '%d:' % movie_id
+            print >> output, '%d:' % movie_id
             last_movie_id = movie_id
-        print >> f, format % predictor(movie_id,user_id)
-    f.close()
+        print >> output, format % predictor(movie_id,user_id)
+
+#==== Data index ===============================================================
+
+class Index(object):
+    def __init__(self, indexpath, datpath, recordtype):
+        # load the index file
+        self._key2index = cPickle.load(open(indexpath,'rb'))
+        # open the data file and mmap it
+        f = open(datpath, 'rb+')
+        self._datfile = datfile = mmap.mmap(f.fileno(), path(f.name).size,
+                                            access=mmap.ACCESS_READ)
+        fromfile = recordtype.fromfile
+        self._readrecord = lambda: fromfile(datfile)
+        
+    def __getitem__(self, key):
+        self._datfile.seek(self._key2index[key])
+        return self._readrecord()
+    
+    def __len__(self):
+        return len(self._key2index)
+    
+    def iterkeys(self):
+        return self._key2index.iterkeys()
+
+    def itervalues(self):
+        self._datfile.seek(0)
+        # iterate over all the records one after the other; no need to seek() 
+        for _ in xrange(len(self._key2index)):
+            yield self._readrecord()
+    
+    def iteritems(self):
+        self._datfile.seek(0)
+        for key in self._key2index:
+            yield key,self._readrecord()
+    
+    __iter__ = iterkeys
+
+    def keys(self): return list(self.iterkeys())
+    def values(self): return list(self.itervalues())
+    def items(self): return list(self.iteritems())
         
     
-
-
 if __name__ == '__main__':
     import sys
     d = RatedDataset(sys.argv[1])
@@ -179,18 +226,14 @@ if __name__ == '__main__':
     if 0:
         predictor = lambda m,u: 3.6
         print d.rmse(predictor)
-        ##writePredictionFile(sys.argv[1], 'dummy.txt', predictor)
+        ##writePredictions(sys.argv[1], 'dummy.txt', predictor)
+    from pyflix import timeCall
+    
+    #print timeCall('d.users(6)', d.users, 6)
+    q = UnratedDataset(sys.argv[1])
+    print q.movies()
+    
+    print timeCall('d.movies(6)', d.movies, 6)
     
     for m,u,r in d.iterMoviesUsersRatings():
         print m,u,r
-
-    #from setup import timeCall    
-    #from itertools import islice
-    #movies = list(islice(MovieRatings.fromfile(sys.argv[1]), 13))[11:]
-    #dicts = [dict(m.iterValueRatings()) for m in movies]
-    #c = MovieRatings.commonValues(*movies)
-    #print c
-    #print sorted(set(m1.values()) & set(m2.values()))
-    ##print MovieRatings.jointRatings(*movies)# list(Movie.iter_common_values_ratings(m1,m2))
-    #print [d1[k] for k in c]
-    #print [d2[k] for k in c]
